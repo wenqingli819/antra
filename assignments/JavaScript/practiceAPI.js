f37cc09059596f32bf6d0c163d30420493ad2799
Wenqing <wenqingli819@gmail.com>
2021-03-11 01:48:24 -0500

    js day 3

--- assignments/JavaScript/practiceAPI.js
+++ assignments/JavaScript/practiceAPI.js
@@ -8,8 +8,15 @@ async function getUsers() {
             let tbody = document.querySelector("#tbBody");
             tbody.innerHTML = "";
             let length = data.length;
+            let userId = location.hash.replace("#", "");
+
             for (let i = 0; i < length; i++) {
-                let row = `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].username}</td><td>${data[i].email}</td><td>${data[i].address.street},${data[i].address.suite},${data[i].address.city}</td><td>${data[i].address.zipcode}</td><td>${data[i].address.geo.lat}</td><td>${data[i].address.geo.lng}</td><td>${data[i].phone}</td><td>${data[i].website}</td></tr>`
+
+                let cssStyle = '';
+                if (userId == data[i].id) {
+                    cssStyle = 'selected';
+                }
+                let row = `<tr class="${cssStyle}"><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].username}</td><td>${data[i].email}</td><td>${data[i].address.street},${data[i].address.suite},${data[i].address.city}</td><td>${data[i].address.zipcode}</td><td>${data[i].address.geo.lat}</td><td>${data[i].address.geo.lng}</td><td>${data[i].phone}</td><td>${data[i].website}</td></tr>`
 
                 tbody.innerHTML = tbody.innerHTML + row;
             }
